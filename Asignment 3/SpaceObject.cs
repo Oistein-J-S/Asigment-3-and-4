4060eb479bf2ee59c3662e837a7c42f0de822ad3
Kenneth <thejack388@gmail.com>
2018-02-09 16:18:23 +0100

    Unbroke the thing

--- Asignment 3/SpaceObject.cs
+++ Asignment 3/SpaceObject.cs
@@ -5,97 +5,56 @@ namespace SpaceSim
     public class SpaceObject
     {
 
-        protected String name; // OK
-        protected SpaceObject orbits;// OK
-        protected double objectRadius; //NA
-        protected double orbitalRadius; //OK
-        protected double orbitalPeriod; //OK
-        protected double xPos;
-        protected double yPos;
+        private String name; // OK
+        private SpaceObject orbits;// OK
+        private double objectRadius; //NA
+        private double orbitalRadius; //OK
+        private double orbitalPeriod; //OK
+        private double xPos;
+        private double yPos;
+
+        public double YPos { get => yPos; set => yPos = value; }
+        public double XPos { get => xPos; set => xPos = value; }
+        public double OrbitalPeriod { get => orbitalPeriod; set => orbitalPeriod = value; }
+        public double OrbitalRadius { get => orbitalRadius; set => orbitalRadius = value; }
+        public double ObjectRadius { get => objectRadius; set => objectRadius = value; }
+        public SpaceObject Orbits { get => orbits; set => orbits = value; }
+        public string Name { get => name; set => name = value; }
 
         // public SpaceObject(String name) {
         //     this.name = name;
         // }//END Creator1
         public SpaceObject(String name, SpaceObject orbits, double orbitalRadius, double orbitalPeriod, double objectRadius)
         {
-            this.name = name;
-            this.orbits = orbits;
-            this.orbitalRadius = orbitalRadius;
-            this.orbitalPeriod = orbitalPeriod;
-            this.objectRadius = objectRadius;
-            xPos = orbitalRadius;
-            yPos = 0;
+            this.Name = name;
+            this.Orbits = orbits;
+            this.OrbitalRadius = orbitalRadius;
+            this.OrbitalPeriod = orbitalPeriod;
+            this.ObjectRadius = objectRadius;
+            XPos = orbitalRadius;
+            YPos = 0;
     }//END Creator2
 
-        //Setters
-        public void SetName(String name)
-        {
-            this.name = name;
-        }
-        public void SetObjectRadius(double objectRadius)
-        {
-            this.objectRadius = objectRadius;
-        }
-        public void SetOrbitalRadius(double orbitalRadius)
-        {
-            this.orbitalRadius = orbitalRadius;
-        }
-        public void SetOrbitalPeriod(double orbitalPeriod)
-        {
-            this.orbitalPeriod = orbitalPeriod;
-        }
-        public void SetXPos(double xPos)
-        {
-            this.xPos = xPos;
-        }
-        public void SetYPos(double yPos)
-        {
-            this.yPos = yPos;
-        }
-        //Getters
-        public String GetName()
-        {
-            return this.name;
-        }
-        public double GetObjectRadius()
-        {
-            return this.objectRadius;
-        }
-        public double GetOrbitalRadius()
-        {
-            return this.orbitalRadius;
-        }
-        public double GetOrbitalPeriod()
-        {
-            return this.orbitalPeriod;
-        }
-        public double GetXPos()
-        {
-            return xPos;
-        }
-        public double GetYPos()
-        {
-            return yPos;
-        }
+
 
         public virtual void CalculatePosition(double time)
         {
-            double rest = time % orbitalPeriod; // remove multiple orbits
-            double relativeTime = rest / orbitalPeriod; // find % value of completed orbit 
+            double rest = time % OrbitalPeriod; // remove multiple orbits
+            double relativeTime = rest / OrbitalPeriod; // find % value of completed orbit 
             double orbitalDegrees = (double)relativeTime * 360; //multiply by 360 to find degrees moved.
             orbitalDegrees = orbitalDegrees * (Math.PI / 180); //Convert degrees to radians
-            xPos = orbitalRadius * Math.Sin(orbitalDegrees); //Calculate updated x position
-            yPos = orbitalRadius * Math.Cos(orbitalDegrees); //Calculate updated y position.
+            XPos = OrbitalRadius * Math.Sin(orbitalDegrees); //Calculate updated x position
+            YPos = OrbitalRadius * Math.Cos(orbitalDegrees); //Calculate updated y position.
             //x = cos
             //y = sin
         }//END calculatePosition
 
         public virtual void Draw()
         {
-            Console.Write(", Radius:" + objectRadius + "km");
-            Console.Write(", Orbit:" + orbitalRadius + "000 km");
-            Console.Write(", periode:" + orbitalPeriod + "days");
-            Console.Write(", Position: " + xPos + ", " + yPos );
+            Console.Write(", Radius:" + ObjectRadius + "km");
+            Console.Write(", Orbit:" + OrbitalRadius + "000 km");
+            Console.Write(", periode:" + OrbitalPeriod + "days");
+            Console.Write(", Position: " + XPos + ", " + YPos );
             Console.WriteLine();
         }//END Draw
     }//END class SpaceObject
@@ -106,7 +65,7 @@ namespace SpaceSim
         public override void Draw()
         {
             Console.Write("Star: ");
-            Console.Write(name);
+            Console.Write(Name);
             base.Draw();
         }//END Draw
     }//END class Star
@@ -117,8 +76,8 @@ namespace SpaceSim
         public override void Draw()
         {
             Console.Write("Planet: ");
-            Console.Write(name);
-            Console.Write(" Orbits:" + orbits.GetName());
+            Console.Write(Name);
+            Console.Write(" Orbits:" + Orbits.Name);
             base.Draw();
         }//END Draw
     }//END class Planet
@@ -129,8 +88,8 @@ namespace SpaceSim
         public override void Draw()
         {
             Console.Write("Moon: ");
-            Console.Write(name);
-            Console.Write(" Orbits:" + orbits.GetName());
+            Console.Write(Name);
+            Console.Write(" Orbits:" + Orbits.Name);
             base.Draw();
         }//END Draw
     }//END class Moon
@@ -141,8 +100,8 @@ namespace SpaceSim
         public override void Draw()
         {
             Console.Write("DwarfPlanet: ");
-            Console.Write(name);
-            Console.Write(" Orbits:" + orbits.GetName());
+            Console.Write(Name);
+            Console.Write(" Orbits:" + Orbits.Name);
             base.Draw();
         }//END Draw
     }//END class DwarfPlanet
